1418538d5ea5888114b83b40688b532e0f86a2fa
SMV <kraft088@gmail.com>
2022-12-03 23:27:50 +1100

    Add task 00

--- Seminar_02_new/Task_00/Program.cs
+++ Seminar_02_new/Task_00/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(100, 1000); // 100-999
+
+int firstDigit = number / 100; // 1 цифра
+
+int thirdDigit = number % 10; // 3 цифра
+
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine("Исходное число: " + number);
+Console.WriteLine("Первая и третья цифра числа: " + result);
